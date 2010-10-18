24aafd30f1a0869b04e4f9424c9816ca3b88b0dc
sturdy <>
2010-10-18 15:02:49 +0000

    fixed vector initialization

--- SUSY/AnalysisNtuplePAT/plugins/analyses/diJets/ntuplePragmas.h
+++ SUSY/AnalysisNtuplePAT/plugins/analyses/diJets/ntuplePragmas.h
@@ -3,39 +3,33 @@
 #include <string>
 
 #include <Math/LorentzVector.h>
+#include "TLorentzVector.h"
 
-//Thanks Ted
+//For some reason this is necessaray, would like to not have to include it
+using namespace ROOT;
 
-typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > LorentzV;
-typedef std::vector<LorentzV>                                     LorentzVs;
-typedef std::map<std::string, bool>                               trigger_b;
-typedef std::map<std::string, int>                                trigger_i;
-typedef std::map<std::string, std::vector<float> >                jetcorrs;
+typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > LorentzP4;
+typedef std::vector<LorentzP4>                                    LorentzP4s;
+typedef std::map<std::string, bool>                               stringtobool;
+typedef std::map<std::string, int>                                stringtoint;
+typedef std::map<std::string, std::vector<float> >                stringtovfloat;
 
 #ifdef __CINT__
 
-#pragma link C++ typedef  LorentzV;
-#pragma link C++ typedef  LorentzVs;
-#pragma link C++ typedef  trigger_b;
-#pragma link C++ typedef  trigger_i;
-#pragma link C++ typedef  jetcorrs;
+#pragma link C++ typedef  LorentzP4;
+#pragma link C++ typedef  LorentzP4s;
+#pragma link C++ typedef  stringtobool;
+#pragma link C++ typedef  stringtoint;
+#pragma link C++ typedef  stringtovfloat;
 
-//#pragma link C++ class    LorentzV+;
-#pragma link C++ class    LorentzVs+;
-#pragma link C++ class    trigger_b+;
-#pragma link C++ class    trigger_i+;
-#pragma link C++ class    jetcorrs+;
+#pragma link C++ class    LorentzP4s+;
+#pragma link C++ class    LorentzP4s::iterator;
+#pragma link C++ class    stringtobool+;
+#pragma link C++ class    stringtobool::iterator;
+#pragma link C++ class    std::pair<std::string,bool>+;
+#pragma link C++ class    stringtovfloat+;
+#pragma link C++ class    stringtovfloat::iterator;
+#pragma link C++ class    std::pair<std::string,std::vector<float> >+;
 
-#pragma link C++ class    std::map<std::string,std::vector<float> >+;
-#pragma link C++ class    std::map<std::string,bool>+;
-#pragma link C++ class    std::map<std::string,int>+;
-#pragma link C++ class    std::vector<TLorentzVector>+;
-
-//#pragma link C++ class    std::vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > >+;
-
-//#pragma link C++ class    std::map<const std::string,std::vector<float> >+;
-//#pragma link C++ class    std::map<const std::string,bool>+;
-//#pragma link C++ class    std::map<const std::string,int>+;
-//#pragma link C++ class    std::vector<const TLorentzVector>+;
 
 #endif
